9dff8737f11095d846c398b30f54dd49d0eca177
Hibiki Okada <4617016@ed.tus.ac.jp>
2020-03-26 16:45:01 +0900

    modified install.sh

--- install.sh
+++ install.sh
@@ -8,3 +8,7 @@ do
   ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
 done
 echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)
+
+ln -s ~/.vim $XDG_CONFIG_HOME/nvim
+ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+echo $(tput setaf 4)neovim config file linkage complete!. ✔︎$(tput sgr0)
