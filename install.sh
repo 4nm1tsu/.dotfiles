1791297f882b765dad466aea149b7f93d9c7829d
Hibiki Okada <4617016@ed.tus.ac.jp>
2020-10-06 02:07:06 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ DOT_DIRECTORY=~/.dotfiles
 FT_DIRECTORY=~/42tokyo
 cd ${DOT_DIRECTORY}
 
+failed=0
 for f in .??*
 do
   [ ${f} = ".git" ] && continue
@@ -12,13 +13,27 @@ do
     ln -snfv ${DOT_DIRECTORY}/${f} ${FT_DIRECTORY}/${f}
   else
     ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
+    if [ $? -ne 0 ]; then
+      failed=1
+    fi
   fi
 done
-echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)
+if [ $failed -eq 0 ]; then
+  echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)
+fi
 
+failed=0
 ln -snfv ${DOT_DIRECTORY}/norminette.vim ~/.cache/dein/repos/github.com/w0rp/ale/ale_linters/c/
+if [ $? -ne 0 ]; then
+  failed=1
+fi
 ln -snfv ${DOT_DIRECTORY}/norminette.vim ~/.cache/dein/.cache/.vimrc/.dein/ale_linters/c
-echo $(tput setaf 4)Deploy norminette.vim complete!. ✔︎$(tput sgr0)
+if [ $? -ne 0 ]; then
+  failed=1
+fi
+if [ $failed -eq 0 ]; then
+  echo $(tput setaf 4)Deploy norminette.vim complete!. ✔︎$(tput sgr0)
+fi
 #ln -s ~/.vim $XDG_CONFIG_HOME/nvim
 #ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
 #echo $(tput setaf 4)neovim config file linkage complete!. ✔︎$(tput sgr0)
